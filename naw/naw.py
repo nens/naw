81f3dc1e7f8958026f9a5fb1d76ba4a3308106cd
Arjan Verkerk <arjan.verkerk@gmail.com>
2016-03-11 11:09:13 +0100

    Add argparse and some other tweaking.

--- naw/naw.py
+++ naw/naw.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
+"""
+Retrieve data from the presence monitor or set a telephone number.
+"""
 
 from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import absolute_import
 from __future__ import division
 
+import argparse
 import json
 import sys
 import re
@@ -14,14 +18,20 @@ try:
 except ImportError:
     import urllib2 as request
 
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib2 import urlencode
 
-def print_phone_number(pattern, data):
-    if sys.platform.startswith('win'):
-        GREEN = '{}'
-        RED = '{}'
-    else:
-        GREEN = '\x1b[1m\x1b[32m{}\x1b(B\x1b[m'
-        RED = '\x1b[1m\x1b[31m{}\x1b(B\x1b[m'
+if sys.platform.startswith('win'):
+    GREEN = '{}'
+    RED = '{}'
+else:
+    GREEN = '\x1b[1m\x1b[32m{}\x1b(B\x1b[m'
+    RED = '\x1b[1m\x1b[31m{}\x1b(B\x1b[m'
+
+
+def print_phone_number(data, pattern):
 
     TEMPLATE = '{SHORTNAME:<32}{SHORTNUMBER:>12}{MOBIEL:>13}  {PRESENCE:<20}'
 
@@ -54,62 +64,67 @@ def print_phone_number(pattern, data):
             print(text)
 
 
-def set_telephone(pattern, data):
+def set_telephone(data, pattern, telephone, yes):
+    "Set telephone for the first record where the name field matches pattern."
     for elem in data:
         name = elem["NAAM"]
-        if re.search(pattern, name, flags=re.IGNORECASE):
-            number_prompt_text = "Type the new number for {}: \n".format(name)
+        match = re.search(pattern, name, flags=re.IGNORECASE)
+        if match:
+            break
 
-            try:
-                number = raw_input(number_prompt_text)
-            except NameError:
-                number = input(number_prompt_text)
+    if yes:
+        confirmed = True
+    elif match:
+        confirm_template = "Change the number for {} to {}? [y/N]:\n"
+        confirm_message = confirm_template.format(name, telephone)
+        try:
+            response = raw_input(confirm_message)
+        except NameError:
+            response = input(confirm_message)
 
-            confirm_text = "Change the number for {} to {}? [y/N]:\n".format(
-                name,
-                number
-            )
+        confirmed = response == 'y'
 
-            try:
-                confirm = raw_input(confirm_text)
-            except NameError:
-                confirm = input(confirm_text)
-
-            if confirm == 'y':
-                change_url = request.urlopen((
-                    'http://buildbot.lizardsystem.nl/cgi-bin/'
-                    'set_telephone?id={}&number={}'.format(
-                        elem['id'],
-                        number
-                    )))
-                print(change_url.read().decode('utf-8'))
-                change_url.close()
-            else:
-                print('Changing nothing for nobody')
+    if match and confirmed:
+        url = 'http://buildbot.lizardsystem.nl/cgi-bin/set_telephone'
+        query_dict = {'id': elem['id'], 'number': telephone}
+        query_string = urlencode(query_dict).encode('ascii')
+        url_file = request.urlopen(url, query_string)
+        response = url_file.read().decode('utf-8')
+        if 'is nu %s' % telephone in response:
+            print(GREEN.format('Telephone successfully changed.'))
+        else:
+            print(RED.format('It didn\'t work out. Sorry.'))
+    else:
+        print('Changing nothing for nobody.')
 
 
-def main():
-    url_file = request.urlopen(
-        'http://buildbot.lizardsystem.nl/gis/aanwezigheid.json'
-    )
+def naw(pattern, telephone, yes):
+    url = 'http://buildbot.lizardsystem.nl/gis/aanwezigheid.json'
+    url_file = request.urlopen(url)
     json_str = url_file.read().decode('utf-8')
     url_file.close()
 
     data = json.loads(json_str)
 
-    try:
-        first_argument = sys.argv[1]
-        if first_argument == '--set-telephone':
-            set_telephone(sys.argv[2], data)
-        elif first_argument == '--help':
-            print('''
-USAGE: naw [NAME]
+    if telephone:
+        set_telephone(data=data, pattern=pattern,
+                      telephone=telephone, yes=yes)
+        return
+    print_phone_number(data=data, pattern=pattern)
 
-OPTIONS:
---set-telephone NAME -- Changes the phone number
-            ''')
-        else:
-            pattern = first_argument
-            print_phone_number(pattern, data)
-    except IndexError:
-        print_phone_number('', data)
+
+def get_parser():
+    """ Return argument parser. """
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('pattern', metavar='PATTERN')
+    parser.add_argument('-s', '--set-telephone',
+                        dest='telephone',
+                        help='Change the phone number')
+    parser.add_argument('-y', '--assume-yes', dest='yes', action='store_true')
+    return parser
+
+
+def main():
+    """ Call naw with args from parser. """
+    kwargs = vars(get_parser().parse_args())
+    naw(**kwargs)
